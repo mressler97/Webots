603422fab40df4b3165b8a2737d7983318586bb8
Michael Ressler <36997670+mressler97@users.noreply.github.com>
2020-04-30 19:46:11 -0600

    Update final_controller.py

--- scribbler/controllers/final_controller/final_controller.py
+++ scribbler/controllers/final_controller/final_controller.py
@@ -2,18 +2,37 @@
 #CSCI 455 Final Project Controller
 
 from controller import Robot, Compass
-import math
+import math, sys
 from math import pi as M_PI
 
 TIME_STEP = 64
 robot = Robot()
 compass = Compass("compass")
+compass.enable(TIME_STEP)
 
 ds = []
 dsNames = ['ds_right', 'ds_left']
 
-compass.enable(TIME_STEP)
 
+wheels = []
+wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
+
+avoidObstacleCounter = 0
+finstate = 0
+start_state = 1
+turnCounter = 0.0
+
+for i in range(2):
+    ds.append(robot.getDistanceSensor(dsNames[i]))
+    ds[i].enable(TIME_STEP)
+
+for i in range(4):
+    wheels.append(robot.getMotor(wheelsNames[i]))
+    wheels[i].setPosition(float('inf'))
+    wheels[i].setVelocity(0.0)
+    
+
+# calculate bearing from compass vector list    
 def get_bearing_in_degrees():
     north = compass.getValues()
     rad = math.atan2(north[0], north[2])
@@ -22,21 +41,14 @@ def get_bearing_in_degrees():
         bearing = bearing + 360.0;
     return bearing
     
+# set speeds of motors    
+def set_motor_speed(wheels, leftSpeed, rightSpeed):
+    wheels[0].setVelocity(leftSpeed)
+    wheels[1].setVelocity(rightSpeed)
+    wheels[2].setVelocity(leftSpeed)
+    wheels[3].setVelocity(rightSpeed)
+    
 
-for i in range(2):
-    ds.append(robot.getDistanceSensor(dsNames[i]))
-    ds[i].enable(TIME_STEP)
-wheels = []
-wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
-for i in range(4):
-    wheels.append(robot.getMotor(wheelsNames[i]))
-    wheels[i].setPosition(float('inf'))
-    wheels[i].setVelocity(0.0)
-avoidObstacleCounter = 0
-
-finstate = 0
-start_state = 1
-turnCounter = 0.0
 
 while robot.step(TIME_STEP) != -1:
     direction = get_bearing_in_degrees()
@@ -52,8 +64,7 @@ while robot.step(TIME_STEP) != -1:
             leftSpeed = 2.4
             rightSpeed = 0.75
             if 90 > direction > 45:
-                turnCounter += 1
-                          
+                turnCounter += 1                        
         else: # make left turn
             if finstate == 0:
                 leftSpeed = 0.75
@@ -68,9 +79,7 @@ while robot.step(TIME_STEP) != -1:
                 if turnCounter != 9:
                     turnCounter += 1
                 else:
-                    finstate = 1
-                    
-                
+                    finstate = 1           
     else: # go straight again
         leftSpeed = 2.0
         rightSpeed = 2.0
@@ -78,14 +87,17 @@ while robot.step(TIME_STEP) != -1:
         avoidObstacleCounter -= 1
         leftSpeed = 0.0
         rightSpeed = 0.0
-    else:  # 
+        set_motor_speed(wheels, leftSpeed, rightSpeed)
+        compass.disable()
+        print("Complete!")
+        sys.exit(0)
+    else:  # set speeds if no wall
         for i in range(2):
             if ds[i].getValue() < 950.0:
                 avoidObstacleCounter = 100
-    wheels[0].setVelocity(leftSpeed)
-    wheels[1].setVelocity(rightSpeed)
-    wheels[2].setVelocity(leftSpeed)
-    wheels[3].setVelocity(rightSpeed)
+                
+    set_motor_speed(wheels, leftSpeed, rightSpeed)
+
     
 
    
